e5fc846a67a874617180a41ade52fa411e8876d3
joão pedro silva <130461387+jprime617@users.noreply.github.com>
2024-10-21 20:48:00 -0300

    commit login/cadastro

--- mobile/app-spotifake/app/index.jsx
+++ mobile/app-spotifake/app/index.jsx
@@ -1,37 +1,53 @@
 import React from 'react';
-import { View, Text, Pressable, TextInput, StyleSheet } from 'react-native';
+import { View, Text, Pressable, TextInput, StyleSheet, StatusBar } from 'react-native';
 import { useState, useEffect } from 'react';
 import { Link } from 'expo-router'
 
 export default function App() {
     const [email, setEmail] = useState('')
     const [senha, setSenha] = useState('')
+    const [press, setPress] = useState(false)
     return (
         <View style={styles.container}>
-            <TextInput
-                placeholder='Email'
-                value={email}
-                onChangeText={(vish) => setEmail(vish)}
+            <StatusBar hidden={true} />
+            <View style={styles.caixatitulo}>
+                <Text style={styles.titulo}>SpotiFake</Text>
+                <Text style={{color: '#FFFF', marginTop: 70, fontWeight: 'bold'}}>Login</Text>
+            </View>
+            <View style={styles.caixainput}>
+                <TextInput
+                    placeholder='Email'
+                    value={email}
+                    onChangeText={(vish) => setEmail(vish)}
+                    style={styles.input}
+                    placeholderTextColor='gray'
 
-            />
-            <TextInput
-                placeholder='Senha'
-                value={senha}
-                onChangeText={(vish) => setSenha(vish)}
+                />
+                <TextInput
+                    placeholder='Senha'
+                    value={senha}
+                    onChangeText={(vish) => setSenha(vish)}
+                    style={styles.input}
+                    placeholderTextColor='gray'
 
-            />
-            <Pressable onPress={() => { console.log(email); console.log(senha) }}>
-                <Text>
-                    Nossa
-                </Text>
-            </Pressable>
-            <Link href={'/spotifake/registro'} asChild>
-                <Pressable>
-                    <Text>
-                        Crie Sua Conta
+                />
+                <Pressable style={[styles.botao, press && styles.bpress]}
+                    onPress={() => { console.log(email); console.log(senha) }}
+                    onPressIn={() => setPress(true)}
+                    onPressOut={() => setPress(false)}
+                >
+                    <Text style={{color: '#FFFF', fontSize: 20, fontWeight: 'bold'}}>
+                        Login
                     </Text>
                 </Pressable>
-            </Link>
+                <Link href={'/spotifake/registro'} asChild>
+                    <Pressable style={styles.link}>
+                        <Text style={{ textDecorationLine: 'underline', color: '#FFFF' }}>
+                            Crie Sua Conta
+                        </Text>
+                    </Pressable>
+                </Link>
+            </View>
         </View>
     )
 }
@@ -41,6 +57,55 @@ const styles = StyleSheet.create({
         flex: 1,
         display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        backgroundColor: '#250902'
+    },
+    botao: {
+        backgroundColor: '#800E13',
+        padding: 15,
+        borderRadius: 20,
+        width: 150,
+        height: 80,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    bpress: {
+        backgroundColor: '#38040E'
+    },
+    input: {
+        backgroundColor: '#640D14',
+        height: 50,
+        width: 300,
+        marginBottom: 30,
+        borderRadius: 30,
+        color: '#FFFF',
+        paddingLeft: 20,
+        textAlign: 'left'
+    },
+    titulo: {
+        color: '#FFFF',
+        fontSize: 40,
+        fontWeight: 'bold'
+
+    },
+    caixatitulo: {
+        // backgroundColor: 'gray',
+        width: '100%',
+        height: 100,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        marginTop: 50
+    },
+    caixainput: {
+        // backgroundColor: 'blue',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        width: '100%',
+        flex: 1.5,
+        marginTop: 50
+    },
+    link: {
+        padding: 20,
     }
 })
--- mobile/app-spotifake/app/spotifake/registro.jsx
+++ mobile/app-spotifake/app/spotifake/registro.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import { View, Text, Pressable, TextInput, StyleSheet } from 'react-native';
+import { View, Text, Pressable, TextInput, StyleSheet, StatusBar } from 'react-native';
 import { useState } from 'react';
 
+const Salvar = (info) => {
+    if(!info.nome || !info.sobrenome || !info.email || !info.senha || !info.dataNascimento){
+        console.log('Ta faltando para ai irmao')
+        return
+    }else{
+        console.log(info)
+    }
+}
+
 export default function Registro() {
+    const [press, setPress] = useState(false)
     const [formData, setFormData] = useState({
         'nome': '',
         'sobrenome': '',
@@ -12,36 +22,56 @@ export default function Registro() {
     })
     return (
         <View style={styles.container}>
-            <TextInput
-                placeholder='Nome'
-                value={formData.nome}
-                onChangeText={(alek) => setFormData({ ...formData, nome: alek })}
-            />
-            <TextInput
-                placeholder='Sobrenome'
-                value={formData.sobrenome}
-                onChangeText={(alek) => setFormData({ ...formData, sobrenome: alek })}
-            />
-            <TextInput
-                placeholder='Email'
-                value={formData.email}
-                onChangeText={(alek) => setFormData({ ...formData, email: alek })}
-            />
-            <TextInput
-                placeholder='Senha'
-                value={formData.senha}
-                onChangeText={(alek) => setFormData({ ...formData, senha: alek })}
-            />
-            <TextInput
-                placeholder='Data de Nascimento'
-                value={formData.dataNascimento}
-                onChangeText={(alek) => setFormData({ ...formData, dataNascimento: alek })}
-            />
-            <Pressable onPress={() => console.log(formData)}>
-                <Text>
-                    Nossa
-                </Text>
-            </Pressable>
+            <StatusBar hidden={true} />
+            <View style={styles.caixatitulo}>
+                <Text style={styles.titulo}>Registro</Text>
+            </View>
+            <View style={styles.caixainput}>
+                <TextInput
+                    style={styles.input}
+                    placeholder='Nome'
+                    value={formData.nome}
+                    onChangeText={(alek) => setFormData({ ...formData, nome: alek })}
+                    placeholderTextColor='gray'
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='Sobrenome'
+                    value={formData.sobrenome}
+                    onChangeText={(alek) => setFormData({ ...formData, sobrenome: alek })}
+                    placeholderTextColor='gray'
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='Email'
+                    value={formData.email}
+                    onChangeText={(alek) => setFormData({ ...formData, email: alek })}
+                    placeholderTextColor='gray'
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='Senha'
+                    value={formData.senha}
+                    onChangeText={(alek) => setFormData({ ...formData, senha: alek })}
+                    placeholderTextColor='gray'
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder='Data de Nascimento'
+                    value={formData.dataNascimento}
+                    onChangeText={(alek) => setFormData({ ...formData, dataNascimento: alek })}
+                    placeholderTextColor='gray'
+                />
+                <Pressable style={[styles.botao, press && styles.bpress]}
+                    onPress={() => Salvar(formData)}
+                    onPressIn={() => setPress(true)}
+                    onPressOut={() => setPress(false)}
+                >
+                    <Text style={{ color: '#FFFF', fontSize: 20, fontWeight: 'bold' }}>
+                        Registrar
+                    </Text>
+                </Pressable>
+            </View>
 
         </View>
     )
@@ -52,6 +82,51 @@ const styles = StyleSheet.create({
         flex: 1,
         display: 'flex',
         justifyContent: 'center',
-        alignItems: 'center'
-    }
+        alignItems: 'center',
+        backgroundColor: '#250902'
+    },
+    botao: {
+        backgroundColor: '#800E13',
+        padding: 15,
+        borderRadius: 20,
+        width: 150,
+        height: 80,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    bpress: {
+        backgroundColor: '#38040E'
+    },
+    input: {
+        backgroundColor: '#640D14',
+        height: 50,
+        width: 300,
+        marginBottom: 30,
+        borderRadius: 30,
+        color: '#FFFF',
+        paddingLeft: 20,
+        textAlign: 'left'
+    },
+    titulo: {
+        color: '#FFFF',
+        fontSize: 40,
+        fontWeight: 'bold'
+
+    },
+    caixatitulo: {
+        // backgroundColor: 'gray',
+        width: '100%',
+        height: 100,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 0.5,
+        marginTop: 55
+    },
+    caixainput: {
+        // backgroundColor: 'blue',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        width: '100%',
+        flex: 1.7
+    },
 })
