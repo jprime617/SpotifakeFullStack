4aceae05032aa38550c17c4a02dff8cca4c27f9a
joão pedro silva <130461387+jprime617@users.noreply.github.com>
2024-10-22 12:23:21 -0300

    coomi final

--- server/index.js
+++ server/index.js
@@ -1,25 +1,53 @@
 import Express from "express";
+import { criarTabelas, User } from "./db.js";
+import bcryptjs from 'bcryptjs'
 
 const app = Express()
 app.use(Express.json())
+// criarTabelas()
 
-app.post('/registro', (req, res) => {
-    const {nome, sobrenome, email, senha, dataNascimento} = req.body
-    if(!nome || !sobrenome || !email || !senha || !dataNascimento) {
+app.post('/registro', async (req, res) => {
+    const { nome, sobrenome, email, senha, dataNascimento, cpf } = req.body
+    if (!nome || !sobrenome || !email || !senha || !dataNascimento) {
         res.send('tem que preencher tudo cabaço')
         return
     }
-    
-    res.send(`Nome: ${nome} ${sobrenome}\n Email: ${email}\n Senha: ${senha}\n Data De Nascimento: ${dataNascimento}`)
+    const emailExist = await User.findOne({where: { email:email } })
+    if (emailExist) {
+        return res.send('Esse Email já Está Cadastrado')
+    }
+    // const cpfExist = await User.findOne({where: { cpf:cpf }})
+    // if (cpfExist) {
+    //     return res.send('Esse CPF já Está Cadastrado')
+    // }
+
+
+
+
+    const senhaCriptografada = bcryptjs.hashSync(senha, 10)
+    const teste = await User.create({ nome, sobrenome, email, senha:senhaCriptografada, dataNascimento, cpf })
+
+
+
+    res.send(`Nome: ${nome} ${sobrenome}\n Email: ${email}\n Senha: ${senha}\n Data De Nascimento: ${dataNascimento}\n CPF: ${cpf}\n Usuario Criado`)
 })
 
-app.post('/login', (req, res) => {
+app.post('/login', async (req, res) => {
     const { email, senha, } = req.body
-    if(!email || !senha ) {
+    if (!email || !senha) {
         res.send('tem que preencher tudo cabaço')
         return
     }
-    
+
+    const userExist = await User.findOne({where: { email:email } })
+    if (!userExist) {
+        return res.send('Esse Usuario Não Existe')
+    }
+    const senhaValida = bcryptjs.compareSync(senha, userExist.senha)
+    if(!senhaValida){
+        return res.send('Senha invalida')
+    }
+
 
     res.send(`Email: ${email}\n Senha: ${senha}`)
 })
