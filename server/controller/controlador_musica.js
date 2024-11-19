50bae62b10b2778769ddd7773cbe11a9f33055cd
joão pedro silva <130461387+jprime617@users.noreply.github.com>
2024-11-19 10:44:38 -0300

    nossa meu

--- server/controller/controlador_musica.js
+++ server/controller/controlador_musica.js
@@ -2,8 +2,8 @@ import { Artista } from "../db.js";
 
 const getMusica = async (req, res) => {
     const info = req.body
-    const receba = await Artista.findAll({ where: {nome: info.body}})
-    res.send("Nossa Alek")
+    const receba = await Artista.findAll({ where: {nome: info.nome}})
+    res.json(receba)
 }
 
 export { getMusica }
